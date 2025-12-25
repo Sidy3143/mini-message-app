4faaee0e73e3206dc01653e73d1981a562ebe869
Sidy3143 <188754494+Sidy3143@users.noreply.github.com>
2025-12-25 15:37:07 +0100

    added environment variable support for port configuration and improved file structure

--- app.js
+++ app.js
@@ -3,17 +3,12 @@ const express = require("express");
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.set("view engine", "ejs");
 
 app.use(express.static('public'));
-
-app.listen(3000, (error) => {
-  if (error) {
-    throw error;
-  }
-
-  console.log(`listening on port 3000!`);
-});
+app.use(express.urlencoded({ extended: true }));
 
 const messages = [
   {
@@ -36,7 +31,6 @@ app.get("/new", (req, res) =>
     res.render('form')
 );
 
-app.use(express.urlencoded({ extended: true }));
 
 app.post("/new", (req, res) => {
     messageText = req.body.message;
@@ -45,10 +39,19 @@ app.post("/new", (req, res) => {
     messages.push({ text: messageText, user: messageUser, added: new Date() });
     res.redirect("/");
 })
+
 app.post("/delete", (req, res) => {
     const index = parseInt(req.body.index);
     if (index >= 0 && index < messages.length) {
         messages.splice(index, 1);
     }
     res.redirect("/");
+});
+
+app.listen(port, (error) => {
+  if (error) {
+    throw error;
+  }
+
+  console.log(`listening on port ${port}!`);
 });
